23ae55a3ef7b322551da9f83cc6a967d37251754
kaosat.dev <kaosat.dev@gmail.com>
2023-11-10 16:02:07 +0100

    chore(): updated/ fixed all examples

--- examples/advanced/game/in_game.rs
+++ examples/advanced/game/in_game.rs
@@ -9,6 +9,7 @@ use bevy_gltf_blueprints::GameWorldTag;
 pub fn setup_game(
     mut commands: Commands,
     game_assets: Res<GameAssets>,
+    models: Res<Assets<bevy::gltf::Gltf>>,
     mut next_game_state: ResMut<NextState<GameState>>,
 ) {
     println!("setting up all stuff");
@@ -17,10 +18,11 @@ pub fn setup_game(
         brightness: 0.2,
     });
     // here we actually spawn our game world/level
-
+    
     commands.spawn((
         SceneBundle {
-            scene: game_assets.world.clone(),
+            // note: because of this issue https://github.com/bevyengine/bevy/issues/10436, "world" is now a gltf file instead of a scene
+            scene: models.get(game_assets.world.id()).expect("main level should have been loaded").scenes[0].clone(),
             ..default()
         },
         bevy::prelude::Name::from("world"),
