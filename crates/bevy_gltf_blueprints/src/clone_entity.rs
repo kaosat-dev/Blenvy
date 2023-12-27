20b34dde20d22f5bb53a3e6dbb86458f485cea4b
Mark Moissette <kaosat.dev@gmail.com>
2023-12-27 13:14:13 +0100

    feat(bevy_gltf_blueprints): improved error handling for clone_entity (#82)
    * feat(bevy_gltf_blueprints): improved error handling for clone_entity
     * you will now get component name for components that have not been registered & thus cannot be cloned
    * added a small example (in examples/bevy_gltf_blueprints/basic), just press 'U' at runtime to try to spawn an entity with unregistered components
    * closes #81

--- crates/bevy_gltf_blueprints/src/clone_entity.rs
+++ crates/bevy_gltf_blueprints/src/clone_entity.rs
@@ -18,29 +18,31 @@ impl CloneEntity {
     // - the source or destination entity do not exist
     fn clone_entity(self, world: &mut World) {
         let components = {
-            let registry = world.get_resource::<AppTypeRegistry>().unwrap().read();
+            let registry = world
+                .get_resource::<AppTypeRegistry>()
+                .expect("the world should have a type registry")
+                .read();
 
             world
                 .get_entity(self.source)
-                .unwrap()
+                .expect("source entity should exist")
                 .archetype()
                 .components()
                 .map(|component_id| {
-                    world
+                    let component_info = world
                         .components()
                         .get_info(component_id)
-                        .unwrap()
-                        .type_id()
-                        .unwrap()
-                })
-                .map(|type_id| {
-                    // println!("type_id {:?}", type_id);
-                    registry
-                        .get(type_id)
-                        .unwrap()
-                        .data::<ReflectComponent>()
-                        .unwrap()
-                        .clone()
+                        .expect("component info should be available");
+
+                    let type_id = component_info.type_id().unwrap();
+                    let type_id = registry.get(type_id).expect(
+                        format!(
+                            "cannot clone entity: component: {:?} is not registered",
+                            component_info.name()
+                        )
+                        .as_str(),
+                    );
+                    return type_id.data::<ReflectComponent>().unwrap().clone();
                 })
                 .collect::<Vec<_>>()
         };
@@ -51,7 +53,9 @@ impl CloneEntity {
                 .unwrap()
                 .clone_value();
 
-            let mut destination = world.get_entity_mut(self.destination).unwrap();
+            let mut destination = world
+                .get_entity_mut(self.destination)
+                .expect("destination entity should exist");
 
             component.apply_or_insert(&mut destination, &*source);
         }
--- examples/bevy_gltf_blueprints/basic_scene_components/src/core/camera/camera_replace_proxies.rs
+++ examples/bevy_gltf_blueprints/basic_scene_components/src/core/camera/camera_replace_proxies.rs
@@ -6,16 +6,23 @@ use bevy::prelude::*;
 
 use super::CameraTrackingOffset;
 
-
 #[derive(Component, Reflect, Default, Debug)]
 #[reflect(Component)]
 pub struct SSAOSettings;
 
 pub fn camera_replace_proxies(
     mut commands: Commands,
-    mut added_cameras: Query<(Entity, &mut Camera, Option<&BloomSettings>, Option<&SSAOSettings>), (Added<Camera>, With<CameraTrackingOffset>)>,
+    mut added_cameras: Query<
+        (
+            Entity,
+            &mut Camera,
+            Option<&BloomSettings>,
+            Option<&SSAOSettings>,
+        ),
+        (Added<Camera>, With<CameraTrackingOffset>),
+    >,
 
-    added_bloom_settings : Query<&BloomSettings, Added<BloomSettings>>,
+    added_bloom_settings: Query<&BloomSettings, Added<BloomSettings>>,
     added_ssao_settings: Query<&SSAOSettings, Added<SSAOSettings>>, // Move to camera
 ) {
     for (entity, mut camera, bloom_settings, ssao_setting) in added_cameras.iter_mut() {
@@ -24,27 +31,25 @@ pub fn camera_replace_proxies(
         commands
             .entity(entity)
             .insert(DebandDither::Enabled)
-            .insert(Tonemapping::BlenderFilmic)
-            ;
+            .insert(Tonemapping::BlenderFilmic);
 
         // we only inject the scene_level bloom settings if there are no settings already on the Camera
         if bloom_settings.is_none() {
-            for bloom_settings in added_bloom_settings.iter(){
-                commands
-                    .entity(entity)
-                    .insert(BloomSettings {
-                        intensity: bloom_settings.intensity,
-                        composite_mode: BloomCompositeMode::Additive,
-                        ..default()
-                    });
+            for bloom_settings in added_bloom_settings.iter() {
+                commands.entity(entity).insert(BloomSettings {
+                    intensity: bloom_settings.intensity,
+                    composite_mode: BloomCompositeMode::Additive,
+                    ..default()
+                });
             }
         }
-       
+
         if ssao_setting.is_none() {
-            for _ in added_ssao_settings.iter(){
+            for _ in added_ssao_settings.iter() {
                 commands.insert_resource(Msaa::Off); // when using SSAO, you cannot use Msaa
-    
-                commands.entity(entity)
+
+                commands
+                    .entity(entity)
                     .insert(ScreenSpaceAmbientOcclusionBundle::default())
                     .insert(TemporalAntiAliasBundle::default());
             }
