09ed6f72b08c13d00019a0fd4748f4ef683a22f7
kaosat.dev <kaosat.dev@gmail.com>
2023-12-31 11:59:01 +0100

    feat(tools/auto_export): added same logic for nested blueprints as with main scenes to handle nested  collections & empties

--- tools/gltf_auto_export/blueprints.py
+++ tools/gltf_auto_export/blueprints.py
@@ -22,7 +22,7 @@ def generate_blueprint_hollow_scene(blueprint_collection, library_collections, a
     # TODO also add the handling for "template" flags, so that instead of creating empties we link the data from the sub collection INTO the parent collection
     # copies the contents of a collection into another one while replacing blueprint instances with empties
     # if we have combine_mode set to "Inject", we take all the custom attributed of the nested (1 level only ! unless we use 'deepMerge') custom attributes and copy them to this level 
-    def copy_hollowed_collection_into(source_collection, destination_collection):
+    def copy_hollowed_collection_into(source_collection, destination_collection, parent_empty=None):
         for object in source_collection.objects:
             combine_mode = object['Combine'] if 'Combine' in object else collection_instances_combine_mode
 
@@ -48,18 +48,35 @@ def generate_blueprint_hollow_scene(blueprint_collection, library_collections, a
 
                 for k, v in object.items():
                     empty_obj[k] = v
+                if parent_empty is not None:
+                    empty_obj.parent = parent_empty
             else:
-                destination_collection.objects.link(object)
+                if parent_empty is not None:
+                    object.parent = parent_empty
+                    destination_collection.objects.link(object)
+                else:
+                    object['___linked'] = True
+                    destination_collection.objects.link(object)
 
         # for every sub-collection of the source, copy its content into a new sub-collection of the destination
         for collection in source_collection.children:
+            original_name = collection.name
+            collection.name = original_name + "____bak"
+            collection_placeholder = make_empty3(original_name, [0,0,0], [0,0,0], [1,1,1], destination_collection)
+
+            if parent_empty is not None:
+                collection_placeholder.parent = parent_empty
+
+            copy_hollowed_collection_into(collection, destination_collection, collection_placeholder)
+
+            """
             copy_collection = bpy.data.collections.new(collection.name + "____collection_export")
             # save the newly created collection for later reuse
             temporary_collections.append(copy_collection)
 
             # copy & link objects
             copy_hollowed_collection_into(collection, copy_collection)
-            destination_collection.children.link(copy_collection)
+            destination_collection.children.link(copy_collection)"""
 
     copy_hollowed_collection_into(blueprint_collection, temp_scene_root_collection)
 
@@ -82,15 +99,20 @@ def clear_blueprint_hollow_scene(temp_scene, original_collection, temporary_coll
     restore_original_names(original_collection)
 
     # remove empties (only needed when we go via ops ????)
-    root_collection = temp_scene.collection 
-    scene_objects = [o for o in root_collection.objects]
-    for object in scene_objects:
+    temp_root_collection = temp_scene.collection 
+    temp_scene_objects = [o for o in temp_root_collection.objects]
+    for object in temp_scene_objects:
         if object.type == 'EMPTY':
             if hasattr(object, "SpawnHere"):
                 bpy.data.objects.remove(object, do_unlink=True)
             else: 
                 bpy.context.scene.collection.objects.unlink(object)
-            #bpy.data.objects.remove(object, do_unlink=True)
+                if '___linked' in object:
+                    del object['___linked']
+                else:
+                    bpy.data.objects.remove(object, do_unlink=True)
+        else: 
+            bpy.context.scene.collection.objects.unlink(object)
 
     # remove temporary collections
     for collection in temporary_collections:
