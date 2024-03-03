64b26b2a70dbf4bb848e61a5c92b44bf1cbd85e4
Jan Hohenheim <jan@hohenheim.ch>
2024-03-03 23:33:21 +0100

    Remove dead code

--- examples/common/src/core/mod.rs
+++ examples/common/src/core/mod.rs
@@ -1,9 +1,6 @@
 pub mod camera;
 pub use camera::*;
 
-pub mod lighting;
-pub use lighting::*;
-
 //pub mod relationships;
 //pub use relationships::*;
 
@@ -22,6 +19,6 @@ use bevy::prelude::*;
 pub struct CorePlugin;
 impl Plugin for CorePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((LightingPlugin, CameraPlugin, PhysicsPlugin));
+        app.add_plugins((CameraPlugin, PhysicsPlugin));
     }
 }
