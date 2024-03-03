8a174c6c8b2e728059004050cb0198278b8c7dc2
Jan Hohenheim <jan@hohenheim.ch>
2024-03-03 23:27:02 +0100

    Remove dead code

--- examples/common/src/core/lighting/mod.rs
+++ examples/common/src/core/lighting/mod.rs
@@ -1,16 +1,11 @@
-mod lighting_replace_proxies;
-use lighting_replace_proxies::*;
-
 use bevy::pbr::NotShadowCaster;
 use bevy::prelude::*;
 
 pub struct LightingPlugin;
 impl Plugin for LightingPlugin {
     fn build(&self, app: &mut App) {
-        app.register_type::<BlenderBackgroundShader>()
-            .register_type::<BlenderShadowSettings>()
+        app
             // FIXME: adding these since they are missing
-            .register_type::<NotShadowCaster>()
-            .add_systems(PreUpdate, lighting_replace_proxies);
+            .register_type::<NotShadowCaster>();
     }
 }
