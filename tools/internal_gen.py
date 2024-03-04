c95eead100251c90773124075b5e4dd12f22f1c3
kaosat.dev <kaosat.dev@gmail.com>
2024-03-04 14:15:30 +0100

    feat(tools): (re) generating gltf files for examples by script works !  * hack-ish, reuses our pytest boilerplate, but good enough for now

--- tools/internal_gen.py
+++
@@ -1,8 +0,0 @@
-import bpy
-
-def test_fake():
-    auto_export_operator = bpy.ops.export_scenes.auto_gltf
-    stored_settings = bpy.data.texts[".gltf_auto_export_settings"] 
-    print("OHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-    print("export settings", stored_settings.as_string())
-    #auto_export_operator(direct_mode = True)
