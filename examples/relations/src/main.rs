565fda6953e2acca3aea5a6f9b7de85dceb038e3
DasLixou <einlixou@gmail.com>
2024-08-21 15:32:37 +0200

    better example

--- examples/relations/src/main.rs
+++ examples/relations/src/main.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use blenvy::{BlenvyPlugin, BlueprintInfo, GameWorldTag, HideUntilReady, SpawnBlueprint};
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
 pub struct TupleRelations(Entity); // TODO: Serialization on blender side currently is broken
 
@@ -21,6 +21,7 @@ fn main() {
         .register_type::<TupleRelations>()
         .register_type::<BigRelations>()
         .add_systems(Startup, setup_game)
+        .add_systems(Update, (print_names, print_tuple_relations))
         .run();
 }
 
@@ -32,4 +33,16 @@ fn setup_game(mut commands: Commands) {
         HideUntilReady, // only reveal the level once it is ready
         GameWorldTag,
     ));
+}
+
+fn print_names(query: Query<(Entity, &Name), Added<Name>>) {
+    for (entity, name) in &query {
+        info!("[EXAMPLE] {name} is {entity}");
+    }
+}
+
+fn print_tuple_relations(query: Query<(&Name, &TupleRelations), Added<TupleRelations>>) {
+    for (name, r) in &query {
+        info!("[EXAMPLE] {name} has the relation {r:?}")
+    }
 }
